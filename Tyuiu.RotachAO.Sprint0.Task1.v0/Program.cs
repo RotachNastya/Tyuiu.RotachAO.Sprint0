b00db32ca598e4c515f9918b762892f6d90446be
RotachAO@outlook.com <RotachAO@outlook.com>
2024-09-18 22:04:44 +0500

    Task1

---
+++ Tyuiu.RotachAO.Sprint0.Task1.v0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.RotachAO.Sprint0.Task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            string name = "Настя";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня зовут  " + name + " мне " + age + " лет! ");
+            Console.ReadKey();
+        }
+    }
+}
