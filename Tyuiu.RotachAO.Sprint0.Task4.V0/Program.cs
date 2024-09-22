078fbedc33e24ede09f0222075310ac2ec788ad5
RotachAO@outlook.com <RotachAO@outlook.com>
2024-09-22 17:04:08 +0500

    Task4

---
+++ Tyuiu.RotachAO.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.RotachAO.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
